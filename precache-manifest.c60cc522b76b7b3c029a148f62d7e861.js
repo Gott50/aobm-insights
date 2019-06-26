94a36006bd41cb6169577e772fcd51d9402fa50d
Gott50 <xxGott50xx@gmail.com>
2019-06-26 20:25:10 +0200

    Updates

---
+++ precache-manifest.c60cc522b76b7b3c029a148f62d7e861.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c3c6d3cf8ef0d452d3bec0cba30252",
+    "url": "/aobm-insights/index.html"
+  },
+  {
+    "revision": "35409ac857b7df9ec973",
+    "url": "/aobm-insights/static/css/main.6bf34bdc.chunk.css"
+  },
+  {
+    "revision": "dafb43cc8e7bf01f0927",
+    "url": "/aobm-insights/static/js/2.9857981d.chunk.js"
+  },
+  {
+    "revision": "35409ac857b7df9ec973",
+    "url": "/aobm-insights/static/js/main.bd391362.chunk.js"
+  },
+  {
+    "revision": "f51622db3275a2b420bd",
+    "url": "/aobm-insights/static/js/runtime~main.4ec5d0b4.js"
+  }
+]);
